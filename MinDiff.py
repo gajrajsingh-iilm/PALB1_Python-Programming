f34eb595d6c33b80707d7e64f34b99c91859367a
gajrajsingh-iilm <gajraj.singh@iilm.edu>
2026-02-10 10:39:15 +0530

    Create MinDiff.py

---
+++ MinDiff.py
@@ -0,0 +1,12 @@
+#User function Template for python3
+
+class Solution:
+
+    def findMinDiff(self, arr,M):
+        # code here
+        arr.sort()
+        min_diff = float('inf')
+        for i in range(len(arr)-M+1):
+            diff = arr[i+M-1]-arr[i]
+            min_diff=min(diff,min_diff)
+        return min_diff
